dffc968af0f6230e0144fa6291b8ea347bf75f78
minho <maxkmh712@gmail.com>
2021-10-06 21:34:40 +0900

    [CREATE] core, users, orders, products app models.py

---
+++ orders/models.py
@@ -0,0 +1,57 @@
+
+from django.db       import models
+
+from products.models import Product, Size
+from users.models    import User
+from core.models     import TimeStamp
+
+
+class OrderStatus(models.Model):
+  status = models.CharField(max_length=45)
+
+  class Meta:
+    db_table = 'order_statuses'
+
+class Order(TimeStamp):
+  user            = models.ForeignKey(User, on_delete=models.CASCADE)
+  status          = models.ForeignKey(OrderStatus, on_delete=models.CASCADE)
+  address         = models.CharField(max_length=1000)
+  zip_code        = models.CharField(max_length=45)
+  recipient       = models.CharField(max_length=45)
+  recipient_phone = models.CharField(max_length=45)
+  total_price     = models.DecimalField(max_digits=5)
+
+  class Meta:
+    db_table = 'orders'
+
+class OrderItemStatus(TimeStamp):
+  status = models.CharField(max_length=45)
+
+  class Meta:
+    db_table = 'order_item_statuses'
+
+class OrderProduct(TimeStamp):
+  order             = models.ForeignKey(Order, on_delete=models.CASCADE)
+  product           = models.ForeignKey(Product, on_delete=models.CASCADE)
+  size              = models.ForeignKey(Size, on_delete=models.CASCADE)
+  order_item_status = models.ForeignKey(OrderItemStatus, on_delete=models.CASCADE)
+  count             = models.IntegerField(default=1)
+
+  class Meta:
+    db_table = 'order_products'
+
+class Shipment(TimeStamp):
+  order           = models.ForeignKey(Order, on_delete=models.CASCADE)
+  tracking_number = models.CharField(max_length=200)
+  shipment_date   = models.DateField()
+
+  class Meta:
+    db_table = 'shipments'
+
+class ShipmentItem(models.Model):
+  order    = models.ForeignKey(Order, on_delete=models.CASCADE)
+  shipment = models.ForeignKey(Shipment, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'shipment_items'
+
---
+++ products/models.py
@@ -0,0 +1,79 @@
+from django.db   import models
+
+from core.models import TimeStamp
+
+class GenderCategory(TimeStamp):
+  name = models.CharField(max_length=45)
+
+  class Meta:
+    db_table = 'gender_categories'
+
+class MainCategory(TimeStamp):
+  gender_category = models.ForeignKey(GenderCategory, on_delete=models.CASCADE)
+  name            = models.CharField(max_length=45)
+
+  class Meta:
+    db_table = 'main_categories'
+
+class SubCategory(TimeStamp):
+  main_category = models.ForeignKey(MainCategory, on_delete=models.CASCADE)
+  name          = models.CharField(max_length=45)
+
+  class Meta:
+    db_table = 'sub_categories'
+
+class Collection(models.Model):
+  name = models.CharField(max_length=45)
+
+  class Meta:
+    db_table = 'colletions'
+
+class Product(TimeStamp):
+  sub_category  = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+  colletion     = models.ForeignKey(Collection, on_delete=models.CASCADE)
+  name          = models.CharField(max_length=45)
+  price         = models.DecimalField(max_digits=5)
+  color         = models.CharField(max_length=45)
+  description   = models.CharField(max_length=1000)
+  length        = models.CharField(max_length=45)
+  fit           = models.CharField(max_length=45)
+  configuration = models.CharField(max_length=100)
+  is_new        = models.BooleanField(default=1)
+  is_conscious  = models.BooleanField(default=0)
+  soft_deleted  = models.BooleanField(default=0)
+
+  class Meta:
+    db_table = 'products'
+
+class Size(models.Model):
+  size = models.CharField(max_length=45)
+
+  class Meta:
+    db_table = 'sizes'
+
+class ProductSize(models.Model):
+  product = models.ManyToManyField(Product)
+  size    = models.ManyToManyField(Size)
+
+  class Meta:
+    db_table = 'products_sizes'
+
+class Image(models.Model):
+  url     = models.CharField(max_length=500)
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'images'
+
+class Material(models.Model):
+  name = models.CharField(max_length=45)
+
+  class Meta:
+    db_table = 'materials'
+
+class ProductMaterial(models.Model):
+  product  = models.ManyToManyField(Product)
+  material = models.ManyToManyField(Material)
+
+  class Meta:
+    db_table = 'products_materials'
