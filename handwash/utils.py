ed5f49fcfc09ff7c56240d8aba657ebbb15cbb6e
thisisempty <thisisemptyyy@gmail.com>
2021-12-01 23:33:26 +0900

    [Refactioring] 환경변수 수정

--- handwash/utils.py
+++ handwash/utils.py
@@ -2,8 +2,8 @@ import jwt
 
 from django.http    import JsonResponse
 
-from my_settings    import SECRET_KEY
-from users.models   import User
+from handwash.settings import SECRET_KEY
+from users.models      import User
 
 def login_decorator(func):
   def wrapper(self, request, *args, **kwargs):
